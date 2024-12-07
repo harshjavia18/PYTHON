8b51f5ad1c1516d62e3b26c567cd2be3f452aed3
Harsh Javia <143782214+harshjavia18@users.noreply.github.com>
2024-12-07 21:20:50 +0530

    Add files via upload

---
+++ IF_CONDITION.py
@@ -0,0 +1,7 @@
+n = input("Enter a number: ")   #Take a input from the user
+n = int(n)                      #Convert into integer if the input is not int
+if n % 2 == 0 :                 #checking the condition
+    print("Even")                #printing the output if true other else statement will execute
+else:
+    print("Odd")
+
